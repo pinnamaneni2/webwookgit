92eeb9a837b7a242133acb11fb4c43b18922fbe1
spinna904(Satish) <spinna904@cable.comcast.com>
2023-02-14 16:57:21 -0500

    first commit

---
+++ src/test/java/com/test/webhookclassIT.java
@@ -0,0 +1,8 @@
+package com.test;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class webhookclassIT extends webhookclassTest {
+    // Execute the same tests but in packaged mode.
+}
