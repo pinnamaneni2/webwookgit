26fea20c9d91be1e50f5c76456b2235bdda10464
spinna904(Satish) <spinna904@cable.comcast.com>
2023-02-15 10:52:52 -0500

    test commit

--- src/main/java/com/test/webhookclass.java
+++ src/main/java/com/test/webhookclass.java
@@ -12,6 +12,7 @@ public class webhookclass {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         System.out.println("123");
+        System.out.println("123");
         return "Hello RESTEasy";
     }
 }
